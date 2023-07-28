ee1e33f6ec8213b5c0c8112dacf04fcffb26f25d
Enes ÜNLÜ <89146799+enes9103@users.noreply.github.com>
2023-07-28 15:34:03 +0300

    feat: tutor_1 created project structure

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res)=>{
+    res.send('INDEX PAGE')
+})
+
+
+app.listen(port, () => {
+  console.log(`Application running on port: ${port}`);
+});
