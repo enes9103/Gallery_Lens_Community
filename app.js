ea1a929dfeedd3c02be5b6e3409e5e57d467d893
Enes ÜNLÜ <89146799+enes9103@users.noreply.github.com>
2023-07-28 16:42:55 +0300

    updated: pages and partial structure

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+//ejs template engine
+app.set('view engine', 'ejs')
+
+//static files middleware
+app.use(express.static('public'))
+
 app.get('/', (req, res)=>{
-    res.send('INDEX PAGE')
+    res.render('index')
 })
 
+app.get('/about', (req, res)=>{
+    res.render('about')
+})
 
 app.listen(port, () => {
   console.log(`Application running on port: ${port}`);
