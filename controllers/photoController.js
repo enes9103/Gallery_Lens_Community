fffd38023edea19d7b43f03037b267ac43dc3944
Enes ÜNLÜ <89146799+enes9103@users.noreply.github.com>
2023-08-11 15:01:44 +0300

    feat: updated router detail page and url params session 8

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -19,7 +19,8 @@ const getAllPhotos = async (req, res) => {
   try {
     const photos = await Photo.find({});
     res.status(200).render("photos", {
-      photos, link: 'photos'
+      photos,
+      link: "photos",
     });
   } catch (error) {
     res.status(500).json({
@@ -29,4 +30,19 @@ const getAllPhotos = async (req, res) => {
   }
 };
 
-export { createPhoto, getAllPhotos };
+const getAPhoto = async (req, res) => {
+  try {
+    const photo = await Photo.findById({ _id: req.params.id });
+    res.status(200).render("photo", {
+      photo,
+      link: "photos",
+    });
+  } catch (error) {
+    res.status(500).json({
+      succeded: false,
+      error,
+    });
+  }
+};
+
+export { createPhoto, getAllPhotos, getAPhoto };
