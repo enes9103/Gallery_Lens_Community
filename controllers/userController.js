7a3cb2dfd2df2facbb108bdabaf109c4bdd9f5fa
Enes ÜNLÜ <89146799+enes9103@users.noreply.github.com>
2023-08-17 15:46:36 +0300

    feat: login structure

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 import User from "../models/userModel.js";
+import bcrypt from "bcrypt";
 
 const createUser = async (req, res) => {
+  console.log("req", req);
   try {
     const user = await User.create(req.body);
     res.status(201).json({
@@ -15,4 +17,40 @@ const createUser = async (req, res) => {
   }
 };
 
-export { createUser };
+const loginUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    console.log("req.body", req.body);
+
+    const user = await User.findOne({ username });
+
+    let same = false;
+
+    if (user) {
+      same = await bcrypt.compare(password, user.password);
+      console.log("same", same);
+    } else {
+      res.status(401).json({
+        succeded: false,
+        error: "There is no such user.",
+      });
+    }
+
+    if (same) {
+      res.status(200).send("You are logged in");
+    } else {
+      return res.status(401).json({
+        succeded: false,
+        error: "Password are not matched",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      succeded: false,
+      error,
+    });
+  }
+};
+
+export { createUser, loginUser };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,5 +4,6 @@ import * as userController from "../controllers/userController.js";
 const router = express.Router();
 
 router.route("/register").post(userController.createUser); //localhost/users/register
+router.route("/login").post(userController.loginUser); //localhost/users/login
 
 export default router;
