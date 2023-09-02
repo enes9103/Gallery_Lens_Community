52d0579f88305aaaae779f3dcaadc9f955821b66
Enes ÜNLÜ <89146799+enes9103@users.noreply.github.com>
2023-09-02 20:02:41 +0300

    feat: authorization session 12

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import conn from "./db.js";
+import cookieParser from "cookie-parser"
 import pageRoute from "./routes/pageRoute.js";
 import photoRoute from "./routes/photoRoute.js";
 import userRoute from "./routes/userRoute.js";
