ab65a72441f3ca4e6f4e253cab1ad5464a8f3d0d
Enes ÜNLÜ <89146799+enes9103@users.noreply.github.com>
2023-09-14 10:10:24 +0300

    feat: updated session 14 validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,12 +13,23 @@ const createUser = async (req, res) => {
     }
 
     const user = await User.create(req.body);
-    res.redirect("/login");
+    res.status(201).json({ user: user._id });
   } catch (error) {
-    res.status(500).json({
-      succeded: false,
-      error,
-    });
+    let errors2 = {};
+
+    if (error.code === 11000) {
+      errors2.email = "The Email is already registered";
+    }
+
+    if (error.name === "ValidationError") {
+      Object.keys(error.errors).forEach((key) => {
+        errors2[key] = error.errors[key].message;
+      });
+    }
+
+    console.log("errors2", errors2);
+
+    res.status(400).json(errors2);
   }
 };
 
