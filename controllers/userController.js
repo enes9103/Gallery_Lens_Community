7be5642c512f84c091aba878b88f56e765e0d798
Enes ÜNLÜ <89146799+enes9103@users.noreply.github.com>
2023-09-15 17:06:45 +0300

    feat: updated session 15 photo users relation and load photo

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import Photo from "../models/photoModel.js";
 
 const createUser = async (req, res) => {
   try {
@@ -68,9 +69,11 @@ const createToken = (userId) => {
   });
 };
 
-const getDashboardPage = (req, res) => {
+const getDashboardPage = async (req, res) => {
+  const photos = await Photo.find({ user: res.locals.user._id });
   res.render("dashboard", {
     link: "dashboard",
+    photos,
   });
 };
 
