711beea18ddc8bc1ab7feb9f9e3c1d9ec83d3811
Enes ÜNLÜ <89146799+enes9103@users.noreply.github.com>
2023-09-18 14:02:54 +0300

    feat: added post comment

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ dotenv.config();
 conn();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;  //127.0.0.1:3000
 
 //ejs template engine
 app.set("view engine", "ejs");
